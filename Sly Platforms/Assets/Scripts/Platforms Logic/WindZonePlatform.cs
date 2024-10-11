ba0941d35c414fbb8fe064fc5b7370738ce2351d
ruslan_beresnyov <rusber03102003@gmail.com>
2024-10-12 00:58:59 +0300

    Add wind zone platform

---
+++ Sly Platforms/Assets/Scripts/Platforms Logic/WindZonePlatform.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Implementation of the platform with wind zone where the player is blown in a random direction
+/// </summary>
+public class WindZonePlatform : MonoBehaviour
+{
+    [SerializeField] private float windStrength = 6f;
+
+    private Vector3 windDirection = Vector3.forward;
+
+    private void Awake()
+    {
+        StartCoroutine(GenerateNewWindDirectionEveryFewSeconds());
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<CharacterController>().Move(windDirection * Time.deltaTime);
+            Debug.Log(windDirection);
+        }
+    }
+
+    private IEnumerator GenerateNewWindDirectionEveryFewSeconds()
+    {
+        while (true)
+        {
+            windDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized * windStrength;
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
